68e25a59a6bed91d22761d0d573f3172a1a45751
Amanpreet Singh <apsdehal@gmail.com>
2014-10-04 23:36:14 +0530

    Adds log generator and deletes unnecessary function

--- main.go
+++ main.go
@@ -1,34 +1,12 @@
 package main 
 
 import (
-	"fmt"
 	"os"
 	"bufio"
-	"os/exec"
 )
 
 var outWriter *bufio.Writer 
 var errWriter *bufio.Writer
-
-func logToFile(r Request) {
-	f, err := os.OpenFile("log.txt", os.O_APPEND|os.O_WRONLY, 0777)
-	if err != nil {
-	    panic(err)
-	}
-
-	defer f.Close()
-	format := "%s %s %s %s %d %s %s\n"
-	msg := fmt.Sprintf(format, r.DestIp, r.Time, r.Method, r.Host, r.HTTPType, r.Path, r.UserAgent )
-	if _, err = f.WriteString(msg); err != nil {
-	    panic(err)
-	}
-}
-
-func clearScreen() {
-    cmd := exec.Command("clear")
-    cmd.Stdout = os.Stdout
-    cmd.Run()
-}
 
 func main () {
 	outWriter = bufio.NewWriter(os.Stdout)
